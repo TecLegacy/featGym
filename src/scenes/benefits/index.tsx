5cd51d128e9ccc505d55499a397691ec36bbcbf7
TecLegacy <kshvkumar.kk2@gmail.com>
2023-02-03 17:11:44 +0530

    Fit Section

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -9,6 +9,7 @@ import {
 } from '@heroicons/react/24/outline';
 
 import BenefitsCard from './BenefitsCard';
+import Button from '@/shared/Button';
 
 interface Props {
   setSelectedPage: (value: SelectedPage) => void;
@@ -54,7 +55,16 @@ const Benefits = ({ setSelectedPage }: Props) => {
       >
         <div className={` mx-auto w-full`}>
           {/* HEADING TEXT */}
-          <div>
+          <motion.div
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ amount: 0.5 }}
+            transition={{ duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}
+          >
             <HeadText word="More than just a gym">
               MORE THAN JUST A GYM
             </HeadText>
@@ -64,7 +74,7 @@ const Benefits = ({ setSelectedPage }: Props) => {
               ullam veniam at quia veritatis, earum doloremque amet nemo. Nisi,
               iste!
             </p>
-          </div>
+          </motion.div>
 
           {/* BENEFITS */}
           <motion.div
@@ -85,6 +95,77 @@ const Benefits = ({ setSelectedPage }: Props) => {
               />
             ))}
           </motion.div>
+
+          {/* GYM GRAPHIC & TEXT*/}
+          <div
+            className={`mt-16  items-center justify-between gap-20 md:mt-28 md:flex`}
+          >
+            {/* GYM GRAPHIC  */}
+            <img
+              className={`mx-auto`}
+              alt="benefits-page-graphic"
+              src={BenefitsPageGraphic}
+            />
+            <div>
+              {/* TEXT */}
+              <div>
+                <div className=" relative ">
+                  <div
+                    className={` before: before:absolute before:-top-20 before:-left-20 before:z-[1] before:content-abstractwaves`}
+                  >
+                    <motion.div
+                      initial="hidden"
+                      whileInView="visible"
+                      viewport={{ once: true, amount: 0.5 }}
+                      transition={{ duration: 0.5 }}
+                      variants={{
+                        hidden: { opacity: 0, x: 50 },
+                        visible: { opacity: 1, x: 0 },
+                      }}
+                    >
+                      <HeadText word="millions of happy members getting fit">
+                        MILLION OF HAPPY MEMEBERS GETTINGS{' '}
+                        <span className={` text-primary-500  `}>FIT</span>
+                      </HeadText>
+                    </motion.div>
+                  </div>
+                </div>
+              </div>
+              {/* DESCRIPTION */}
+              <motion.div
+                className={' mb-10   '}
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.5 }}
+                transition={{ delay: 0.2, duration: 0.5 }}
+                variants={{
+                  hidden: { opacity: 0, x: 50 },
+                  visible: { opacity: 1, x: 0 },
+                }}
+              >
+                <p className={` py-8`}>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit,
+                  ipsa. Necessitatibus cumque doloremque quaerat fugiat
+                  pariatur. Id reiciendis, corrupti magnam unde quos rem
+                  repellendus, odit error et distinctio architecto dignissimos
+                  inventore doloribus. Laboriosam libero minima sequi, rerum
+                  facilis deserunt cum, eaque asperiores, facere perspiciatis
+                  tempora.
+                </p>
+                <p className=" mb-6">
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Numquam veritatis libero esse quod nihil ea.
+                </p>
+              </motion.div>
+              {/* BUTTON GRAPHIC */}
+              <div className={` relative `}>
+                <div
+                  className={`before:absolute before:-bottom-20 before:right-40 before:z-[-1] before:content-sparkles`}
+                ></div>
+                <Button setSelectedPage={setSelectedPage}>Join Now</Button>
+              </div>
+            </div>
+          </div>
         </div>
       </motion.div>
     </section>
