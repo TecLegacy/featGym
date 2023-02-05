3843cd5e41af186a191025043ab88eaaaef071f7
TecLegacy <kshvkumar.kk2@gmail.com>
2023-02-05 21:17:27 +0530

    FeatGym, An React & Typescript application

--- src/constants/images.tsx
+++ src/constants/images.tsx
@@ -1,8 +1,7 @@
 import Logos from '@/asset/Featgym.webp';
 // import GymTexts from '@/asset/FeatgymText.svg';
-import GymText from '@/asset/FeatgymText.svg';
+import GymText from '@/asset/featGymText.webp';
 export default {
   GymText,
   Logos,
-  // GymTexts,
 };
